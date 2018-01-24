71234ce3165138ed06813b4506c9dbc0d6853a0b
August <ajokubauskass@gmail.com>
2018-01-24 00:22:46 +0000

    improve request code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,44 @@
-use std::io;
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
 
-fn get_input() -> Result<String, io::Error> {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-    let clean_input = String::from(input.trim());
-    Ok(clean_input)
+// use std::io::{self, Write};
+use futures::{Future, Stream, future};
+use hyper::Client;
+use hyper::client::HttpConnector;
+use tokio_core::reactor::Core;
+
+fn body_to_string(res: hyper::Response) -> Box<Future<Item=String, Error=hyper::error::Error>> {
+    let body = res.body().concat2().map(| x | {
+        match String::from_utf8(x.to_vec()) {
+            Ok(body) => body,
+            Err(_x) => String::from("")
+        }
+    });
+    Box::new(body)
 }
 
-fn get_split_input(line: & String) -> std::str::SplitWhitespace {
-    line.split_whitespace()
-}
-
-fn get_converted_line() -> Vec<i32> {
-    let line = match get_input() {
-        Ok(line) => line,
-        _ => panic!("Could not get first line")
-    };
-    get_split_input(&line).map(| n | { n.parse::<i32>().unwrap() }).collect()
-}
-
-fn shift(list: & Vec<i32>, rotations: usize) -> Vec<i32> {
-    let (left, right) = list.split_at(rotations);
-    let concat = [&right[..], &left[..]].concat();
-    Vec::from(concat)
+fn get(uri: String, client: hyper::Client<HttpConnector>) -> Box<Future<Item=String, Error=hyper::error::Error>>
+{
+    let url = uri.parse::<hyper::Uri>().unwrap();
+    Box::new(client.get(url).and_then(body_to_string))
 }
 
 fn main() {
-    let first_line: Vec<i32> = get_converted_line();
-    let second_line: Vec<i32> = get_converted_line();
+    let mut core = Core::new().unwrap();
 
-    let n_count = first_line[0];
-    let rotations = first_line[1] % n_count;
-    let shifted_list = shift(&second_line, rotations as usize);
-    // println!("");
-    shifted_list.iter().for_each(| n | print!("{} ", n));
+    let task1 = get(String::from("http://httpbin.org/ip"), Client::new(&core.handle()));
+    core.run(task1).and_then(| resp | {
+        println!("{}", resp);
+        Ok(())
+    });
+    
+    let task2 = get(String::from("http://httpbin.org/ip"), Client::new(&core.handle()));
+    core.run(task2).and_then(| resp | {
+        println!("{}", resp);
+        Ok(())
+    });
+    ()
+    // let resp = core.run(task).unwrap();
+    // println!("{}", resp);
 }
