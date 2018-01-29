2328bb19ba0c76de033ec659b7703c9bd6f2567e
Augustinas <augustinass@gmail.com>
2018-01-29 18:07:05 +0000

    save before moving to reqwest

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,31 @@ extern crate tokio_core;
 mod manager;
 use manager::RequestManager;
 
+static TOKEN: &'static str ="EAAMMXECigFsBAFaJP3af4VvUqZBkVVCrRZAATZCqQ8pOPu37reum9eLCOsGskLnmZBvWS2MDKoy2jKBmluTJoH9ZBZBCLtycfrPGVtH69hbvlplnmEl3ssh8ykasEZCVeXxjFeCklWmkyzIUEPmWchbYZAgaX5yMZCjM6OEK7HxFAiwZDZD";
+static BASE_URI: &'static str = "http://graph.facebook.com/v2.11";
+static GROUP_ID: &'static str = "352110954925626";
+/*
+    Graph base url
+    https://graph.facebook.com/v2.11
+
+    https://www.facebook.com/groups/352110954925626/permalink/1150443481759032
+    For single post
+*/
 fn main() {
-    let mut manager = RequestManager::new();
-    let uri = String::from("http://jsonplaceholder.typicode.com/comments?postId=1&&id=1");
-    match manager.get(uri.clone()) {
+    let mut manager = RequestManager::new(
+        String::from(BASE_URI),
+        String::from(GROUP_ID),
+        String::from(TOKEN),
+    );
+    // manager.get_posts();
+    // let uri = String::from("http://jsonplaceholder.typicode.com/comments?postId=1&&id=1");
+    match manager.get_posts() {
         Ok(resp) => println!("1 {}", resp),
         Err(_e) => println!("{:?}", _e)
     };
 
-    match manager.get(uri.clone()) {
-        Ok(resp) => println!("2 {}", resp),
-        Err(_e) => println!("{:?}", _e)
-    };
+    // match manager.get(uri.clone()) {
+    //     Ok(resp) => println!("2 {}", resp),
+    //     Err(_e) => println!("{:?}", _e)
+    // };
 }
--- src/manager.rs
+++ src/manager.rs
@@ -4,7 +4,7 @@ extern crate tokio_core;
 
 // use std::io::{self, Write};
 use futures::{Future, Stream};
-use hyper::Client;
+use hyper::{Client};
 use tokio_core::reactor::Core;
 
 fn body_to_string(res: hyper::Response) -> Box<Future<Item=String, Error=hyper::error::Error>> {
@@ -18,19 +18,30 @@ fn body_to_string(res: hyper::Response) -> Box<Future<Item=String, Error=hyper::
 }
 
 pub struct RequestManager {
-  core: Core
+  core: Core,
+  base_uri: String,
+  group_id: String,
+  token: String,
 }
 
 impl RequestManager {
-  pub fn new() -> RequestManager {
+  pub fn new(base_uri: String, group_id: String, token: String) -> RequestManager {
     let core = Core::new().unwrap();
-    RequestManager { core }
+    RequestManager { core , base_uri, group_id, token }
   }
 
-  pub fn get(&mut self, uri: String) -> Result<String, hyper::Error> {
-    let url = uri.parse::<hyper::Uri>().unwrap();
+  // pub fn get(&mut self, uri: String) -> Result<String, hyper::Error> {
+  //   let url = uri.parse::<hyper::Uri>().unwrap();
+  //   let client = Client::new(&self.core.handle());
+  //   let task = client.get(url).and_then(body_to_string);
+  //   self.core.run(task)
+  // }
+
+  pub fn get_posts(&mut self) -> Result<String, hyper::Error> {
+    let uri_string = format!("{}/{}/feed?access_token={}", self.base_uri, self.group_id, self.token);
+    let uri = uri_string.parse::<hyper::Uri>().unwrap();
     let client = Client::new(&self.core.handle());
-    let task = client.get(url).and_then(body_to_string);
+    let task = client.get(uri).and_then(body_to_string);
     self.core.run(task)
   }
 }
